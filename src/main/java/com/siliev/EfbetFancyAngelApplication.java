c5bc707a14697edd1bf8885392d40ef7f7327731
Stanislav <s.iliev@dimiroma.com>
2020-10-14 08:14:46 +0300

    init

---
+++ src/main/java/com/siliev/EfbetFancyAngelApplication.java
@@ -0,0 +1,13 @@
+package com.siliev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfbetFancyAngelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfbetFancyAngelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siliev/EfbetFancyAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siliev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfbetFancyAngelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
