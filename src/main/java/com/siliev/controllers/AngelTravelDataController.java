835bab944395151f5db12b5890e84d31a9fec0da
Stanislav <s.iliev@dimiroma.com>
2020-10-16 12:33:25 +0300

    adding some more classes

--- src/main/java/com/siliev/controllers/AngelTravelDataController.java
+++ src/main/java/com/siliev/controllers/AngelTravelDataController.java
@@ -3,20 +3,28 @@ package com.siliev.controllers;
 import com.siliev.dto.CountryBordersDto;
 import com.siliev.services.CountryBordersService;
 import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("api/travel/angel")
 public class AngelTravelDataController {
 
-    private final CountryBordersService countryBordersService;
+    @Value("${currency.exchange.rate.url}")
+    private String url;
 
-    public AngelTravelDataController(CountryBordersService countryBordersService) {
+    private final CountryBordersService countryBordersService;
+    private final RestTemplate restTemplate;
+
+    public AngelTravelDataController(CountryBordersService countryBordersService, RestTemplate restTemplate) {
         this.countryBordersService = countryBordersService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/data")
@@ -25,6 +33,11 @@ public class AngelTravelDataController {
         @RequestParam(value = "budgetPerCountry", required = false) Double budgetPerCountry,
         @RequestParam(value = "totalBudget", required = false) Double totalBudget
     ) {
+
+        ResponseEntity<Map> response = restTemplate.getForEntity(url, Map.class);
+
+        Map<String,Double> currencyExchangeRate = (Map<String, Double>) response.getBody().get("rates");
+
         return ResponseEntity.ok().body(countryBordersService.findAllBordersByCountry(startCountry));
     }
 
